a4b4925d6d41aaf9995d63b23c47035ee1e96bbe
Monk Tea <71178046+MonkTeam@users.noreply.github.com>
2021-01-13 22:33:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 jio_saavn_api = "http://127.0.0.1:5000/result/?query="
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001176749967
+sudo_chat_id = "-1001176749967"
 owner_id = 1327934109
